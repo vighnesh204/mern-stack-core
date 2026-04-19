cc6af5bc7f588679c377d20006463bab208163f5
vighnesh204 <borhadevighnesh@gmail.com>
2026-04-19 23:47:08 +0530

    polyfill of map reduce filter

--- JavaScript-interview/1-let-var-const/map-filter-reduce/script.js
+++ JavaScript-interview/1-let-var-const/map-filter-reduce/script.js
@@ -30,3 +30,45 @@ console.log(sum); // 10
 
 
 
+
+// Question 4 : Map Polyfill
+
+Array.prototype.myMap = function (cb) {
+  let temp = [];
+  for (let i = 0; i < this.length; i++) {
+    temp.push(cb(this[i], i, this));
+  }
+
+  return temp;
+};
+
+
+
+// Ques 5 : Filter Polyfill
+
+Array.prototype.myFilter = function (cb) {
+  let temp = [];
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i], i, this)) temp.push(this[i]);
+  }
+
+  return temp;
+};
+
+
+
+// Question 6 : Reduce Polyfill
+
+Array.prototype.myReduce = function (cb, initialValue) {
+  var accumulator = initialValue;
+
+  for (let i = 0; i < this.length; i++) {
+    accumulator = accumulator ? cb(accumulator, this[i], i, this) : this[i];
+  }
+
+  return accumulator;
+};
+
+
+
+
