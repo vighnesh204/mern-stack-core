c260484432c569b43cda3d87e931c5adb305b194
vighnesh204 <borhadevighnesh@gmail.com>
2026-04-21 13:55:26 +0530

    create /login controller and fix bugs

--- Authentication/app.js
+++ Authentication/app.js
@@ -23,14 +23,14 @@ app.get("/", (req, res)=>{
     res.send(`<h1>Auth System</h1>`)
 })
 
-app.post("/register", (req, res)=>{
+app.post("/register", async (req, res)=>{
     try {
         // console.log(req.body);
     // collect data
     const { firstname, lastname, email, password } = req.body;
     
     // validate the data, if exists
-    if(( email && password && firstname && lastname )){
+    if(!( email && password && firstname && lastname )){
         res.status(401).send("All field are required")
     }
 
@@ -71,5 +71,42 @@ app.post("/register", (req, res)=>{
         res.json({
             message: error
         })
+    }
+})
+
+app.post("/login", async (req, res) => {
+    try {
+        // collect information from frontend
+        const { email, password } = req.body;
+        // validate
+        if(!(email && password)){
+            res.status(401).send("email and passsword required")
+        }
+        // check user in database
+        const user = await User.findOne({ email })
+       
+        // match the password
+        // if the user exists 
+        if(user && (await bcrypt.compare(password, user.password))){
+            const token = jwt.sign({id: user._id, email}, "mysecret", { expiresIn: "2h"})
+            user.password = undefined
+            user.token = token
+
+            const options = {
+                expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000 ),
+                httpOnly: true // nobody can manipulate this cookies
+            }
+            res.status(200).cookie("token", token, options).json({
+                success: true,
+                token,
+                user
+            })
+        }   
+
+        // create token and send
+        res.sendStatus(400).send("email or password is incorrect")
+    } catch (error) {
+        console.log(error);
+        
     }
 })
