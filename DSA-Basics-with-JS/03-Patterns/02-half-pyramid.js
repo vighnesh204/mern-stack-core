b5e47130acd573a89c7bef8aea48f9274f83602d
vighnesh204 <borhadevighnesh@gmail.com>
2026-07-04 18:16:42 +0530

    feat: add half-pyramid pattern

---
+++ DSA-Basics-with-JS/03-Patterns/02-half-pyramid.js
@@ -0,0 +1,14 @@
+let n = 4;
+for (let i = 0; i < n; i++) {
+   let row = " ";
+    for (let j = 0; j <= i; j++) {
+        row = row + "* ";
+    }
+    console.log(row)
+}
+
+// Output:
+//  * 
+//  * * 
+//  * * * 
+//  * * * *
