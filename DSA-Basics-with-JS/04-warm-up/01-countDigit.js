e2695dd98795f6743b62edd9f40753054cd68dc7
vighnesh204 <borhadevighnesh@gmail.com>
2026-07-07 17:28:22 +0530

    feat(warm-up): add countDigit.js solution (normal solution)

---
+++ DSA-Basics-with-JS/04-warm-up/01-countDigit.js
@@ -0,0 +1,25 @@
+// write a function that takes a number as input and returns the count of digits in that number.
+
+// Math.floor() method returns the largest integer less than or equal to a given number.
+// Math.floor() method rounds a number DOWNWARDS to the nearest integer, and returns the result.
+// Example: Math.floor(4.9) returns 4, Math.floor(4.5) returns 4
+
+// Math.round() method returns the value of a number rounded to the nearest integer.
+// Example: Math.round(4.9) returns 5, Math.round(4.5) returns 5, Math.round(4.4) returns 4
+
+// Math.ceil() method returns the smallest integer greater than or equal to a given number.
+// Example: Math.ceil(4.1) returns 5, Math.ceil(4.5) returns 5
+
+function countDigit(n){
+    let count = 0;
+    while(n > 0){
+        n = Math.floor(n / 10); 
+        count++;
+    }
+    return count;
+}
+
+let num = 12345;
+
+const result = countDigit(num);
+console.log(result)
