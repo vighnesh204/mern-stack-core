40fe7fd82cfbd4c358f0403a7af249ed466a61e6
vighnesh204 <borhadevighnesh@gmail.com>
2026-07-09 19:40:49 +0530

    feat(warm-up): add reverseInteger.js handling negative numbers

--- DSA-Basics-with-JS/04-warm-up/03-reverseInteger.js
+++ DSA-Basics-with-JS/04-warm-up/03-reverseInteger.js
@@ -1,10 +1,13 @@
 const reverseInteger = (n) => {
+    let isNegative = n < 0;
+    n = Math.abs(n);
+
     let rev = 0;
     while(n > 0){
         let lastDigit = n % 10;
         rev = (rev * 10) + lastDigit;
         n = Math.floor(n / 10);
     }
-    return rev;
+    return isNegative ? -rev : rev;
 }
 console.log(reverseInteger(1234))
